f17b1dbda3710df9bbca25f55d173acf3b82d692
BlueWhiteTen <tasos.bog@gmail.com>
2026-04-28 22:30:51 +0100

    Fix context processor - add sidebar_groups and pending_requests

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,8 +1,18 @@
-from .models import Notification
+from .models import Notification, Group, FriendRequest
 
 
 def sidebar_context(request):
     if request.user.is_authenticated:
         unread = Notification.objects.filter(recipient=request.user, is_read=False).count()
-        return {'unread_notifs': unread}
-    return {'unread_notifs': 0}
+        sidebar_groups = Group.objects.filter(members=request.user).order_by('-created_at')[:10]
+        pending_requests = FriendRequest.objects.filter(to_user=request.user, accepted=False)
+        return {
+            'unread_notifs': unread,
+            'sidebar_groups': sidebar_groups,
+            'pending_requests': pending_requests,
+        }
+    return {
+        'unread_notifs': 0,
+        'sidebar_groups': [],
+        'pending_requests': [],
+    }
